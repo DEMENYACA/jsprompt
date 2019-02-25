bff7770bb351ed8aea5b9bf02c2cbffdf5b3f48f
Demenyaca <demenyaca@gmail.com>
2019-02-25 08:57:53 -0500

    JS prompt

---
+++ Prompt.js
@@ -0,0 +1,13 @@
+var greeting = prompt("What would you like the greeting to be?");
+console.log(greeting);
+var title = document.getElementById("greeting");
+title.innerHTML = greeting;
+
+
+var day = prompt("When is your birthday?");
+var month = document.getElementById("day");
+month.innerHTML = day;
+
+var Thai = prompt ("What is you favorite food?");
+var favorite = document.getElementById("thai");
+favorite.innerHTML= Thai;
